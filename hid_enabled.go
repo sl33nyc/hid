3972a67522b2789aab6594ba69d6a4a9d019a001
Stephen Lee <stephen.lee@fanduel.com>
2022-01-01 16:38:07 -0500

    oops

--- hid_enabled.go
+++ hid_enabled.go
@@ -44,7 +44,6 @@ import "C"
 
 import (
 	"errors"
-	"runtime"
 	"sync"
 	"unsafe"
 )
@@ -163,12 +162,7 @@ func (dev *Device) Write(b []byte) (int, error) {
 		return 0, ErrDeviceClosed
 	}
 	// Prepend a HID report ID on Windows, other OSes don't need it
-	var report []byte
-	//if runtime.GOOS == "windows" {
-	//	report = append([]byte{0x00}, b...)
-	//} else {
-		report = b
-	//}
+	var report []byte = b
 	// Execute the write operation
 	written := int(C.hid_write(device, (*C.uchar)(&report[0]), C.size_t(len(report))))
 	if written == -1 {
